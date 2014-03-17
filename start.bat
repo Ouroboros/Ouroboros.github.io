62eee029009a64a81e4a43ba024449f8b6d0b8a2
アリアンフロッド <Hiromi.Kaede@gmail.com>
2014-03-17 13:00:25 +0800

    .

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@ cd/d "%~dp0"
 set PATH=D:\Dev\ruby\bin\;%PATH%
 set jekyll=D:\Dev\ruby\bin\jekyll.bat
 
-call %jekyll% serve --port 80 --watch --trace
+call %jekyll% serve --port 80 --watch --trace --config _config.yml,_config.local.yml
 exit
